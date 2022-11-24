6f1b09405fa12cd8cd0df8ad2c0e46fbfae42224
onkarr19 <onkarchendage19@gmail.com>
2022-11-24 10:59:28 +0530

    first commit

--- Implementation/pairTrading/pairTradingApp/utils.py
+++ Implementation/pairTrading/pairTradingApp/utils.py
@@ -1,51 +1,51 @@
-import yfinance as yf
+import pickle
+
 import pandas as pd
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sns;
 
 sns.set(style="whitegrid")
-
+plt.style.use('dark_background')
 pd.core.common.is_list_like = pd.api.types.is_list_like
 from pandas_datareader import data as pdr
 
-yf.pdr_override()
 
+pd.core.common.is_list_like = pd.api.types.is_list_like
 start = datetime.datetime(2016, 1, 1)
 end = datetime.datetime(2020, 1, 1)
 
-stockz = sorted(
-    ["EEM", "SPY", "GDX", "XLF", "XOP", "AMLP", "FXI", "QQQ", "EWZ", "EFA", "USO", "HYG", "IAU", "IWM", "XLE", "XLU",
-     "IEMG", "GDXJ", "SLV", "VWO", "XLP", "XLI", "OIH", "LQD", "XLK", "VEA", "TLT", "IEFA", "XLV", "EWJ", "GLD", "IYR",
-     "BKLN", "EWH", "ASHR", "XLB", "RSX", "JNK", "KRE", "XBI", "AGG", "VNQ", "GOVT", "UNG", "IVV", "XLY", "EWT", "PFF",
-     "XLRE", "MCHI", "INDA", "BND", "USMV", "EZU", "SMH", "XRT", "EWY", "IEF", "SPLV", "XLC", "IJR", "VIXY", "EWG",
-     "EWW", "VTI", "VGK", "IBB", "PGX", "VOO", "EMB", "SCHF", "VEU", "SJNK", "EMLC", "XME", "DIA", "EWA", "VCSH",
-     "JPST", "MLPA", "VCIT", "ITB", "ACWI", "KWEB", "EWC", "EWU", "BNDX", "SHY", "VT", "IWD", "VXUS", "MBB", "ACWX",
-     "XHB", "BSV", "SHV", "FEZ", "IWF", "IGSB", "SPYV", "ITOT", "FPE", "FVD", "SHYG", "VYM", "BBJP", "DGRO", "KBE",
-     "VTV", "SPAB", "SPIB", "IWR", "DBC", "BIL", "SPSB", "FLOT", "GLDM", "VIG", "XES", "SCHE", "TIP", "PDBC", "SPYG",
-     "MINT", "SCZ", "SPDW", "PCY", "USHY", "IXUS", "NEAR", "EPI", "SPLG", "HYLB", "AAXJ", "SPEM", "VMBS", "BIV", "QUAL",
-     "ILF", "EWP"])
-
-df = pdr.get_data_yahoo(stockz, start, end)['Close']
+df = pd.read_csv('new_data_with_sectors.csv')
+df = df.query("Date >= '2016-01-01'and Date <= '2020-01-01'")
+df = df.pivot(index='Date', columns='Symbol', values='Close')
+df.columns = ["".join(tup) for tup in df.columns.to_flat_index()]
+df.reset_index()
+df.index = pd.to_datetime(df.index, format="%Y/%m/%d")
+start_date = '2016-01-01'
+end_date = '2020-01-01'
 
 
 def zscore(series):
     return (series - series.mean()) / np.std(series)
 
 
-def visualize(a, b):
-    ratios = df[a] / df[b]
+def visualize(stock1, stock2):
+    ratios = df[stock1] / df[stock2]
     train_test_index = int(len(ratios) * .70)
     train = ratios[:train_test_index]
     test = ratios[train_test_index:]
+
     zscore(ratios).plot(figsize=(12, 6))
     plt.axhline(zscore(ratios).mean())
     plt.axhline(1.0, color='red')
     plt.axhline(-1.0, color='green')
     plt.xlim('2016-01-01', '2020-01-01')
+    plt.legend('Z-Score')
 
-    plt.savefig('pairTradingApp/static/img1.jpg')
+    plt.savefig('./static/img1.jpg')
+    plt.close()
+
 
     ratios_mavg5 = train.rolling(window=5, center=False).mean()
     ratios_mavg60 = train.rolling(window=60, center=False).mean()
@@ -56,9 +56,9 @@ def visualize(a, b):
     plt.plot(ratios_mavg5.index, ratios_mavg5.values)
     plt.plot(ratios_mavg60.index, ratios_mavg60.values)
     plt.legend(['Ratio', '5d Ratio MA', '60d Ratio MA'])
-
     plt.ylabel('Ratio')
-    plt.savefig('pairTradingApp/static/img2.jpg')
+    plt.savefig('./static/img2.jpg')
+    plt.close()
 
     plt.figure(figsize=(12, 6))
     zscore_60_5.plot()
@@ -67,7 +67,8 @@ def visualize(a, b):
     plt.axhline(1.0, color='red', linestyle='--')
     plt.axhline(-1.0, color='green', linestyle='--')
     plt.legend(['Rolling Ratio z-Score', 'Mean', '+1', '-1'])
-    plt.savefig('pairTradingApp/static/img3.jpg')
+    plt.savefig('./static/img3.jpg')
+    plt.close()
 
     plt.figure(figsize=(12, 6))
 
@@ -80,14 +81,14 @@ def visualize(a, b):
     sell[160:].plot(color='r', linestyle='None', marker='^')
     x1, x2, y1, y2 = plt.axis()
     plt.axis((x1, x2, ratios.min(), ratios.max()))
-    plt.xlim('2016-08-15', '2018-07-07')
+    plt.xlim(start_date, end_date)
     plt.legend(['Ratio', 'Buy Signal', 'Sell Signal'])
-    plt.savefig('pairTradingApp/static/img4.jpg')
+    plt.savefig('./static/img4.jpg')
+    plt.close()
 
     plt.figure(figsize=(12, 7))
-    S1 = df[a].iloc[:train_test_index]
-    S2 = df[b].iloc[:train_test_index]
-
+    S1 = df[stock1].iloc[:train_test_index]
+    S2 = df[stock2].iloc[:train_test_index]
     S1[60:].plot(color='b')
     S2[60:].plot(color='c')
     buyR = 0 * S1.copy()
@@ -105,11 +106,21 @@ def visualize(a, b):
     sellR[60:].plot(color='r', linestyle='None', marker='^')
     x1, x2, y1, y2 = plt.axis()
     plt.axis((x1, x2, min(S1.min(), S2.min()), max(S1.max(), S2.max())))
-    plt.ylim(5, 60)
-    plt.xlim('2016-03-22', '2018-07-04')
+    plt.ylim(min(S1.min(), S2.min()) - 100, max(S1.max(), S2.max()) + 100)
+    plt.xlim(start_date, end_date)
 
-    plt.legend([a, b, 'Buy Signal', 'Sell Signal'])
-    plt.savefig('pairTradingApp/static/img5.jpg')
+    plt.legend([stock1, stock2, 'Buy Signal', 'Sell Signal'])
+    plt.savefig('./static/img5.jpg')
+    plt.close()
 
 
-# visualize('XLF', 'XOP')
+# visualize('ITC', 'KOTAKBANK')
+
+def read_pickle():
+    dbfile = open(r'./map.pkl', 'rb')
+    data = pickle.load(dbfile)
+    dbfile.close()
+    return data
+
+
+# data = read_pickle()
