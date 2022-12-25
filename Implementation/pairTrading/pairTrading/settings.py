ed23221282c3a2b4f3ab081c223211724842ff06
Onkar Chendage <onkar@MacBook-Air.local>
2022-12-25 17:02:38 +0530

    update

--- Implementation/pairTrading/pairTrading/settings.py
+++ Implementation/pairTrading/pairTrading/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-el6qqmp(@3*$$$g39a!kol@(p^12ex_4*#*x1f=oyge!sm_x_f"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
